0c89d3fd9cbfc9befccaa25051f0ab47f83cf580
Shawindi dinuruvani <shawindidinuruvani@gmail.com>
2026-06-09 12:17:50 +0530

    add admin middleware and role system

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\AuthController;
-Route::post('/book', [BookingController::class, 'book']);
+
+
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -14,8 +15,15 @@ Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::put('/events/{id}', [EventController::class, 'update']);
 Route::delete('/events/{id}', [EventController::class, 'destroy']);
-Route::middleware('auth:sanctum')->post('/book', [BookingController::class, 'book']);
+Route::post('/book', [BookingController::class, 'book']);
 
+Route::middleware('auth:sanctum')->post('/book', [BookingController::class, 'book']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('admin')->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return "Admin Only";
+    });
 });
